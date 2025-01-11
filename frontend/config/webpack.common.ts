a42452951fb030aea4eba543935dcba19183fa7e
Junseo Kim <semiwest2@gmail.com>
2025-01-11 16:28:57 +0900

    style: Prettier를 사용하여 코드 포맷팅 적용
    - Prettier를 사용하여 프로젝트 전체 코드 스타일 정리
    - 기능 변경 없음, 오직 코드 스타일 수정만 적용

--- frontend/config/webpack.common.ts
+++ frontend/config/webpack.common.ts
@@ -1,39 +1,39 @@
-import * as path from 'path'
-import * as webpack from 'webpack'
+import * as path from 'path';
+import * as webpack from 'webpack';
 // plugin
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const configuration: webpack.Configuration = {
-	// 모듈 해석 방법 설정
-    resolve: {
-        // 생략할 확장자
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  // 모듈 해석 방법 설정
+  resolve: {
+    // 생략할 확장자
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
 
-        // 절대 경로
-        alias : {
-            "@src" : path.resolve(__dirname, '/src/'),
-        },
+    // 절대 경로
+    alias: {
+      '@src': path.resolve(__dirname, '/src/'),
     },
+  },
 
-    entry: "./src/index",
+  entry: './src/index',
 
-    // 로더
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx|js|jsx)$/,
-                use: ["ts-loader"],
-                exclude: /node_modules/,
-            },
-        ],
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', 'public', 'index.html'),
-        }),
-        new webpack.ProvidePlugin({React: "react"}),
+  // 로더
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
     ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', 'public', 'index.html'),
+    }),
+    new webpack.ProvidePlugin({ React: 'react' }),
+  ],
 };
 
 export default configuration;
