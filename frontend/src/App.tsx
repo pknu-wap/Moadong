94aa06e60a1d071174517eb2de8c20309b95406a
Junseo Kim <semiwest2@gmail.com>
2025-01-15 17:33:17 +0900

    chore: Global.styels.ts 설정
    Co-Authored-By: Seongwon Seo <seongwon0903@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import GlobalStyles from './styles/Global.styels';
 
 const App = () => {
-  return <div>Hello Hoj</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>Hello Hoj</div>;
+    </>
+  );
 };
 export default App;
