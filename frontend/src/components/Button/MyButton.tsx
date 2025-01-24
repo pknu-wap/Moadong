e8f2117b4692e4d39b9e4856ddd78e49542a630c
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-01-25 00:59:07 +0900

    feat: 테스트 버튼 컴포넌트 추가

---
+++ frontend/src/components/Button/MyButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const MyButton = (props: {
+  children: React.ReactNode;
+  backgroundColor?: string;
+}) => {
+  return (
+    <button style={{ backgroundColor: props.backgroundColor }}>
+      {props.children}
+    </button>
+  );
+};
