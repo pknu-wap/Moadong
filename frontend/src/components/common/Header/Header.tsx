adec5113032ace541fdb390812b8b7c743dd1c06
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-01-27 16:55:23 +0900

    feat: 헤더 검색창 컴포넌트 추가

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -4,8 +4,14 @@ import * as Styled from './Header.styles';
 const Header = () => {
   return (
     <Styled.HeaderStyles>
-      <Styled.LogoButtonStyles>Moadong</Styled.LogoButtonStyles>
-      <Styled.introduceButtonStyles>모아동 소개</Styled.introduceButtonStyles>
+      <Styled.TextCoverStyles>
+        <Styled.LogoButtonStyles>Moadong</Styled.LogoButtonStyles>
+        <Styled.introduceButtonStyles>모아동 소개</Styled.introduceButtonStyles>
+      </Styled.TextCoverStyles>
+      <Styled.searchBoxStyles>
+        <Styled.searchInputStyles placeholder='어떤 동아리를 찾으세요?' />
+        <Styled.SearchButton />
+      </Styled.searchBoxStyles>
     </Styled.HeaderStyles>
   );
 };
