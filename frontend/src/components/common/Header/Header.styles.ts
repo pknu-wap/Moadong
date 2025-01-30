b5290ed96c4b2b12da02e75735c500251a12adc2
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-01-31 01:53:12 +0900

    feat: 헤더 항상 중앙정렬
    - 화면 넓어져도 1280px 유지

--- frontend/src/components/common/Header/Header.styles.ts
+++ frontend/src/components/common/Header/Header.styles.ts
@@ -15,6 +15,8 @@ export const HeaderContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
 `;
 
 export const TextCoverStyles = styled.div`
