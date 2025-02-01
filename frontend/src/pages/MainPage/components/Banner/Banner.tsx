762249b26bb75bb0a2752fb4dba06289f9a4f6fd
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-01 19:39:34 +0900

    feat: Banner 컴포넌트 추가

--- frontend/src/pages/MainPage/components/Banner/Banner.tsx
+++ frontend/src/pages/MainPage/components/Banner/Banner.tsx
@@ -1,6 +1,12 @@
+import React, { useRef, useState, useEffect } from 'react';
+import * as Styled from './Banner.styles';
+import { BannerProps } from './Banner.styles';
+
 interface BannerComponentProps {
   banners: BannerProps[];
 }
+
+const Banner = ({ banners }: BannerComponentProps) => {
   const slideRef = useRef<HTMLDivElement>(null);
   const [currentSlide, setCurrentSlide] = useState(0);
   const IMG_WIDTH = 1180;
@@ -20,3 +26,35 @@ interface BannerComponentProps {
     if (currentSlide <= 0) return;
     setCurrentSlide((prev) => prev - 1);
   };
+
+  return (
+    <Styled.BannerContainer>
+      <Styled.BannerWrapper>
+        <Styled.ButtonContainer>
+          <Styled.NextButton onClick={moveToPrevSlide}>
+            <img src='/prevButton.png' />
+          </Styled.NextButton>
+          <Styled.NextButton onClick={moveToNextSlide}>
+            <img src='/nextButton.png' />
+          </Styled.NextButton>
+        </Styled.ButtonContainer>
+        <Styled.SlideWrapper ref={slideRef}>
+          {banners.map((banner, index) => (
+            <Styled.Dash
+              key={index}
+              style={{ backgroundImage: `url(${banner.backgroundImage})` }}>
+              <Styled.BannerTextContainer>
+                <Styled.BannerTitle>{banner.title}</Styled.BannerTitle>
+                <Styled.BannerDescription>
+                  {banner.description}
+                </Styled.BannerDescription>
+              </Styled.BannerTextContainer>
+            </Styled.Dash>
+          ))}
+        </Styled.SlideWrapper>
+      </Styled.BannerWrapper>
+    </Styled.BannerContainer>
+  );
+};
+
+export default Banner;
