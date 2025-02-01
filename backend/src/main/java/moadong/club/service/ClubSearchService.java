9e460aea86f065df9b570fc9a779a9ba045180b1
Zepelown <dbswjdqhd12@gmail.com>
2025-02-01 22:59:15 +0900

    chore: 코드 스타일 통일

--- backend/src/main/java/moadong/club/service/ClubSearchService.java
+++ backend/src/main/java/moadong/club/service/ClubSearchService.java
@@ -3,9 +3,7 @@ package moadong.club.service;
 import lombok.AllArgsConstructor;
 import moadong.club.payload.dto.ClubSearchResult;
 import moadong.club.payload.response.ClubSearchResponse;
-import moadong.club.repository.ClubInformationRepository;
 import moadong.club.repository.ClubKeywordSearchRepository;
-import moadong.club.repository.ClubTagRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,9 +12,6 @@ import java.util.List;
 @AllArgsConstructor
 public class ClubSearchService {
     private final ClubKeywordSearchRepository clubKeywordSearchRepository;
-    private final ClubInformationRepository clubInformationRepository;
-    private final ClubTagRepository clubTagRepository;
-
     public ClubSearchResponse searchClubsByKeyword(String keyword) {
         List<ClubSearchResult> clubSearchResults = clubKeywordSearchRepository.searchResult(keyword);
 
