d9f57e8daf353282e20f9981f73d55f93ccf5e98
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-01 19:23:26 +0900

    feat: 배너 컴포넌트 및 스타일 추가
    - 배너 레이아웃 및 텍스트 스타일 정의
    - BannerContainer:  배너 정렬
    - BannerWrapper: 배경 이미지 스타일
    - SlideWrapper: 슬라이드 전환 효과
    - Dash: 슬라이드 영역 크기 설정
    - BannerTextContainer: 제목 및 설명 텍스트 레이아웃
    - BannerTitle, BannerDescription: 텍스트 스타일 설정

--- frontend/src/pages/MainPage/components/Banner/Banner.styles.ts
+++ frontend/src/pages/MainPage/components/Banner/Banner.styles.ts
@@ -1,5 +1,67 @@
+import styled from 'styled-components';
+
 export interface BannerProps {
   backgroundImage?: string;
   title?: string;
   description?: string;
 }
+
+export const BannerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 80px;
+  position: relative;
+`;
+
+export const BannerWrapper = styled.div<BannerProps>`
+  position: relative;
+  width: 1180px;
+  height: 316px;
+  border-radius: 20px;
+  overflow: hidden;
+  background-color: #ffddad;
+  ${({ backgroundImage }) =>
+    backgroundImage &&
+    `
+    background-image : url(${backgroundImage});
+    background-size: cover;
+    background-position: center;
+    `}
+`;
+
+export const SlideWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  transition: all 0.5s ease-in-out;
+`;
+
+export const Dash = styled.div`
+  flex: none;
+  width: 1180px;
+  height: 316px;
+`;
+
+export const BannerTextContainer = styled.div`
+  margin-left: 59px;
+  margin-top: 180px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+export const BannerTitle = styled.h1`
+  font-size: 48px;
+  font-weight: bold;
+  color: #121212;
+  letter-spacing: -0.02em;
+`;
+
+export const BannerDescription = styled.p`
+  font-size: 27px;
+  font-weight: 400;
+  color: #000000;
+  letter-spacing: -0.02em;
+`;
+
