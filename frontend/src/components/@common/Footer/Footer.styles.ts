5216fa068ab1112f043f83dc020cc84336f8199b
Junseo Kim <semiwest2@gmail.com>
2025-02-02 00:54:35 +0900

    feat: Footer 스타일 파일 추가 및 반응형 스타일 적용

---
+++ frontend/src/components/@common/Footer/Footer.styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  text-align: left;
+  font-size: 0.75rem;
+  padding: 0 140px;
+
+  @media (max-width: 499px) {
+    padding: 0 20px;
+  }
+`;
+
+export const Divider = styled.hr`
+  border: none;
+  border-top: 1px solid #c5c5c5;
+`;
+
+export const FooterContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0 30px 0;
+  line-height: 1.25rem;
+  color: #818181;
+
+  @media (max-width: 499px) {
+    font-size: 0.625rem;
+  }
+`;
+
+export const PolicyText = styled.p``;
+
+export const CopyRightText = styled.p``;
+
+export const EmailText = styled.p`
+  a {
+    color: #aaa;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
