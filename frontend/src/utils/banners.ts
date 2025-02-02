e585c8aed9ab1a17f15f1b3ea361b8c95a625eb5
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-02 15:25:30 +0900

    feat: 배너 이미지 및 슬라이드 버튼 export설정
    - 배너이미지는 객체로 전달
    - 슬라이드 버튼은 리스트로 전달

---
+++ frontend/src/utils/banners.ts
@@ -0,0 +1,15 @@
+import FirstSlideImage from '../assets/images/banner_desktop1.png';
+import SecondSlideImage from '../assets/images/banner_desktop2.png';
+import PrevButton from '../assets/images/prevButton.png';
+import NextButton from '../assets/images/nextButton.png';
+
+export const BannerImageList = [
+  {
+    backgroundImage: FirstSlideImage,
+  },
+  {
+    backgroundImage: SecondSlideImage,
+  },
+];
+
+export const SlideButton = [PrevButton, NextButton];
