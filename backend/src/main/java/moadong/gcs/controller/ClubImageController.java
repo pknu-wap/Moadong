292211b5bb9e3fe49b9683c45f9e716ba216c85f
FhRh <yiyop@naver.com>
2025-02-09 19:44:20 +0900

    fix : swagger를 통해 파일업로드 테스트

--- backend/src/main/java/moadong/gcs/controller/ClubImageController.java
+++ backend/src/main/java/moadong/gcs/controller/ClubImageController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import moadong.gcs.dto.ImageDeleteRequest;
 import moadong.gcs.service.ClubImageService;
 import moadong.global.payload.Response;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,10 +29,10 @@ public class ClubImageController {
     private final ClubImageService clubImageService;
 
 
-    @PostMapping("/{clubId}/logo")
+    @PostMapping(value = "/{clubId}/logo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "로고 이미지 업로드", description = "로고 이미지를 저장소에 업로드합니다.")
     public ResponseEntity<?> uploadLogo(@PathVariable String clubId,
-                                        @RequestParam("logo") MultipartFile file) {
+                                        @RequestPart("logo") MultipartFile file) {
         String fileUrl = clubImageService.uploadLogo(clubId, file);
         return Response.ok(fileUrl);
     }
@@ -42,10 +44,10 @@ public class ClubImageController {
         return Response.ok("success delete image");
     }
 
-    @PostMapping("/{clubId}/feeds")
+    @PostMapping(value = "/{clubId}/feeds", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "피드 이미지 업로드", description = "피드 이미지를 저장소에 업로드합니다.")
     public ResponseEntity<?> uploadLogo(@PathVariable String clubId,
-                                        @RequestParam("feeds") List<MultipartFile> files) {
+                                        @RequestPart("feeds") List<MultipartFile> files) {
         clubImageService.uploadFeeds(clubId, files);
         return Response.ok("success upload feeds");
     }
