428f03e507fe676ca5786361b1cde03587cff0f1
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-10 16:21:24 +0900

    feat: Footer추가

--- frontend/src/pages/ClubDetailPage/ClubDetailPagel.tsx
+++ frontend/src/pages/ClubDetailPage/ClubDetailPagel.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/common/Header/Header';
 import InfoBox from '@/pages/ClubDetailPage/components/InfoBox/InfoBox';
 import IntroduceBox from '@/pages/ClubDetailPage/components/IntroduceBox/IntroduceBox';
 import * as Styled from '../MainPage/MainPage.styles';
+import Footer from '@/components/@common/Footer/Footer';
 
 const ClubDetailPage = () => {
   return (
@@ -12,6 +13,7 @@ const ClubDetailPage = () => {
         <InfoBox />
         <IntroduceBox />
       </Styled.PageContainer>
+      <Footer />
     </>
   );
 };
