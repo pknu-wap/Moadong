0fffcf065480a2fcc468e0853714cc8d921f6594
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-12 22:30:48 +0900

    fix: 헤더 조건부 렌더링
    - width가 500px 이내라면 헤더 제거되도록 함

--- frontend/src/pages/ClubDetailPage/ClubDetailPage.tsx
+++ frontend/src/pages/ClubDetailPage/ClubDetailPage.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '@/components/common/Header/Header';
 import InfoTabs from '@/pages/ClubDetailPage/components/InfoTabs/InfoTabs';
 import InfoBox from '@/pages/ClubDetailPage/components/InfoBox/InfoBox';
 import IntroduceBox from '@/pages/ClubDetailPage/components/IntroduceBox/IntroduceBox';
-import * as Styled from '../MainPage/MainPage.styles';
 import Footer from '@/components/@common/Footer/Footer';
+import * as Styled from '@/styles/PageContainer.styles';
 
 const ClubDetailPage = () => {
+  const [showHeader, setShowHeader] = React.useState(window.innerWidth > 500);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowHeader(window.innerWidth > 500);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
-      <Header />
-      <InfoTabs />
+      {showHeader && <Header />}
       <Styled.PageContainer>
         <InfoBox />
         <IntroduceBox />
