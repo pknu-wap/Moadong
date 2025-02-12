33e8e07a37a520cbf733f2e28bb53a51d6721e53
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-12 22:25:05 +0900

    feat: 자동스크롤 훅 추가
    - sectionRefs로 스크롤될 섹션을 배열로 관리
    - scrollTo로 인덱스에 따라 해당 섹션으로 스크롤되도록 함

---
+++ frontend/src/hooks/useAutoScroll.ts
@@ -0,0 +1,23 @@
+import { useRef } from 'react';
+
+const useAutoScroll = () => {
+  const sectionRefs = useRef<(HTMLDivElement | null)[]>(
+    new Array(4).fill(null),
+  );
+
+  const scrollToSection = (index: number) => {
+    if (sectionRefs.current[index]) {
+      const element = sectionRefs.current[index];
+      const yOffset = -100;
+
+      window.scrollTo({
+        top: element.getBoundingClientRect().top + window.scrollY + yOffset,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  return { sectionRefs, scrollToSection };
+};
+
+export default useAutoScroll;
