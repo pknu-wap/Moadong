641b606e036b3dca38c5e83ea91b577dc5e62653
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-12 22:31:24 +0900

    feat: 자동 스크롤 기능 추가
    - 스크롤되어야 하는 컴포넌트에 sectionRefs 전달하여 스크롤 섹션으로 추가

--- frontend/src/pages/ClubDetailPage/ClubDetailPage.tsx
+++ frontend/src/pages/ClubDetailPage/ClubDetailPage.tsx
@@ -5,8 +5,10 @@ import InfoBox from '@/pages/ClubDetailPage/components/InfoBox/InfoBox';
 import IntroduceBox from '@/pages/ClubDetailPage/components/IntroduceBox/IntroduceBox';
 import Footer from '@/components/@common/Footer/Footer';
 import * as Styled from '@/styles/PageContainer.styles';
+import useAutoScroll from '@/hooks/useAutoScroll';
 
 const ClubDetailPage = () => {
+  const { sectionRefs, scrollToSection } = useAutoScroll();
   const [showHeader, setShowHeader] = React.useState(window.innerWidth > 500);
 
   useEffect(() => {
@@ -21,9 +23,10 @@ const ClubDetailPage = () => {
   return (
     <>
       {showHeader && <Header />}
+      <InfoTabs onTabClick={scrollToSection} />
       <Styled.PageContainer>
-        <InfoBox />
-        <IntroduceBox />
+        <InfoBox sectionRefs={sectionRefs} />
+        <IntroduceBox sectionRefs={sectionRefs} />
       </Styled.PageContainer>
       <Footer />
     </>
