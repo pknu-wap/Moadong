ad4e957003db9e752e049f69841c67698731f18e
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-18 12:53:46 +0900

    feat: 지원마감날짜 배지 컴포넌트 추가

---
+++ frontend/src/pages/ClubDetailPage/components/DeadlineBadge/DeadlineBadge.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from './DeadlineBadge.styles';
+
+const DeadlineBadge = () => {
+  return (
+    <Styled.DeadlineBadgeWrapper>
+      <Styled.DeadlineBadgeText>D-10</Styled.DeadlineBadgeText>
+    </Styled.DeadlineBadgeWrapper>
+  );
+};
+
+export default DeadlineBadge;
