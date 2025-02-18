6a4211cca9b436195ceb574495f3156dd068dfa9
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-02-18 11:31:06 +0900

    feat: 메인아이콘 클릭 시 루트로 이동기능 추가
    - useNavigate로 루트로 이동

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import * as Styled from './Header.styles';
 import SearchBox from '@/components/common/SearchBox/SearchBox';
 import MainIcon from '@/assets/images/mainIcon.png';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate('/');
+  };
+
   return (
     <Styled.HeaderStyles>
       <Styled.HeaderContainer>
         <Styled.TextCoverStyles>
           <Styled.LogoButtonStyles>
-            <img src={MainIcon} />
+            <img src={MainIcon} onClick={handleHomeClick} />
           </Styled.LogoButtonStyles>
           <Styled.IntroduceButtonStyles>
             모아동 소개
